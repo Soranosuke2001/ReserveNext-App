1293b33b82f57d9e5c9815461ddba48f7276be26
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-05 21:59:01 -0700

    completed basic outline of search filter page

--- app/page.tsx
+++ app/page.tsx
@@ -70,6 +70,29 @@ export default function Home() {
               </div>
             </div>
           </div>
+          {/* Search Side Bar Component */}
+          <div className="w-5/6">
+            <div className="border-b flex pb-5">
+              <Image src="" alt="" className="w-44 rounded" />
+              <div className="pl-5">
+                <h2 className="text-3xl">Asiana Restaurant Collective</h2>
+                <div className="flex items-start">
+                  <div className="flex mb-2">*****</div>
+                  <p className="ml-2 text-sm">Awesome</p>
+                </div>
+                <div className="mb-9">
+                  <div className="font-light flex text-reg">
+                    <p className="mr-4">$$$</p>
+                    <p className="mr-4">Mexican</p>
+                    <p className="mr-4">Toronto</p>
+                  </div>
+                </div>
+                <div className="text-red-600">
+                  <Link href="">View More Information</Link>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
     </main>
