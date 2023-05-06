7a078fdfec5713753737d3852e8ccafd6745767a
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-06 00:59:40 -0700

    separated the input form from reservation page

---
+++ app/reserve/[slug]/components/Form.tsx
@@ -0,0 +1,50 @@
+import { FC } from "react";
+
+interface FormProps {}
+
+const Form: FC<FormProps> = ({}) => {
+  return (
+    <div className="mt-10 flex flex-wrap justify-between w-[660px]">
+      <input
+        type="text"
+        className="border rounded p-3 w-80 mb-4"
+        placeholder="First Name"
+      />
+      <input
+        type="text"
+        className="border rounded p-3 w-80 mb-4"
+        placeholder="Last Name"
+      />
+      <input
+        type="number"
+        className="border rounded p-3 w-80 mb-4"
+        placeholder="Phone Number"
+      />
+      <input
+        type="email"
+        className="border rounded p-3 w-80 mb-4"
+        placeholder="Email"
+      />
+      <input
+        type="text"
+        className="border rounded p-3 w-80 mb-4"
+        placeholder="Occasion (Optional)"
+      />
+      <input
+        type="text"
+        className="border rounded p-3 w-80 mb-4"
+        placeholder="Special Requests (Optional)"
+      />
+      <button className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
+        Complete Reservation
+      </button>
+      <p className="mt-4 text-small">
+        By clicking "Complete Reservation" you agree to the OpenTable Terms of
+        Use and Privacy Policy. Standard text messages rate may apply. You may
+        opt out of receiving text messages at any time.
+      </p>
+    </div>
+  );
+};
+
+export default Form;
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
 import NavBar from "../../components/NavBar";
+import Form from "./components/Form";
 import Header from "./components/Header";
 
 export default function Reservation() {
@@ -11,47 +10,7 @@ export default function Reservation() {
         <div className="border-t h-screen">
           <div className="py-9 w-3/5 m-auto">
             <Header />
-            {/* Form Component */}
-            <div className="mt-10 flex flex-wrap justify-between w-[660px]">
-              <input
-                type="text"
-                className="border rounded p-3 w-80 mb-4"
-                placeholder="First Name"
-              />
-              <input
-                type="text"
-                className="border rounded p-3 w-80 mb-4"
-                placeholder="Last Name"
-              />
-              <input
-                type="number"
-                className="border rounded p-3 w-80 mb-4"
-                placeholder="Phone Number"
-              />
-              <input
-                type="email"
-                className="border rounded p-3 w-80 mb-4"
-                placeholder="Email"
-              />
-              <input
-                type="text"
-                className="border rounded p-3 w-80 mb-4"
-                placeholder="Occasion (Optional)"
-              />
-              <input
-                type="text"
-                className="border rounded p-3 w-80 mb-4"
-                placeholder="Special Requests (Optional)"
-              />
-              <button className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
-                Complete Reservation
-              </button>
-              <p className="mt-4 text-small">
-                By clicking "Complete Reservation" you agree to the OpenTable
-                Terms of Use and Privacy Policy. Standard text messages rate may
-                apply. You may opt out of receiving text messages at any time.
-              </p>
-            </div>
+            <Form />
           </div>
         </div>
       </main>
