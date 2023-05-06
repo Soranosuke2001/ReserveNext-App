0c776876b6c05c4802dac78525e3680ac8563148
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-05 21:05:05 -0700

    added images and list of reviews

--- app/page.tsx
+++ app/page.tsx
@@ -66,6 +66,49 @@ export default function Home() {
                 memorable one.
               </p>
             </div>
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                10 Photos
+              </h1>
+              <div className="flex flex-wrap">
+                <Image src="" alt="" className="w-56 h-44 mr-1 mb-1" />
+                <Image src="" alt="" className="w-56 h-44 mr-1 mb-1" />
+                <Image src="" alt="" className="w-56 h-44 mr-1 mb-1" />
+                <Image src="" alt="" className="w-56 h-44 mr-1 mb-1" />
+                <Image src="" alt="" className="w-56 h-44 mr-1 mb-1" />
+              </div>
+            </div>
+
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                What 100 People are saying
+              </h1>
+              <div>
+                <div className="border-b pb-7 mb-7">
+                  <div className="flex">
+                    <div className="w-1/6 flex flex-col items-center">
+                      <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
+                        <h2 className="text-white text-2xl">MJ</h2>
+                      </div>
+                      <p className="text-center">Michael Jordan</p>
+                    </div>
+                    <div className="ml-10 w-5/6">
+                      <div className="flex items-center">
+                        <div className="flex mr-5">*****</div>
+                      </div>
+                      <div className="mt-5">
+                        <div className="text-lg font-light">
+                          Laurie was on top of everything! Slow night due to the
+                          snow storm so it worked in our favour to have more one
+                          on one with the staff. Delicious nad well worth the
+                          money.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
