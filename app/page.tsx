c3b7f9e1faea51142c431b4e5c39fe41fe89e416
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-05 20:42:07 -0700

    basic component for each restaurant

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,32 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-<main>Home Page</main>
+    <main className="bg-gray-100 min-h-screen w-screen">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/* Navbar Component */}
+        <nav className="bg-white p-2 flex justify-between">
+          <Link href="" className="font-bold text-gray-700 text-2xl">
+            OpenTable
+          </Link>
+          <div className="flex">
+            <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+              Sign In
+            </button>
+            <button className="border p-1 px-4 rounded text-black">
+              Sign Up
+            </button>
+          </div>
+        </nav>
+
+        {/* Header Component */}
+        <div className="h-96 overflow-hidden">
+          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
+            <h1 className="text-7xl text-white capitalize text-shadow text-center">
+              Milestones Grill (Toronto)
+            </h1>
+          </div>
+        </div>
+      </main>
+    </main>
   );
 }
