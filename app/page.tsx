9185bb64716fd715bfe56311ac1275c062dd1441
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-05 20:23:01 -0700

    completed card component and moved to a html file

--- app/page.tsx
+++ app/page.tsx
@@ -6,56 +6,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        {/* Navbar Component */}
-        <nav className="bg-white p-2 flex justify-between">
-          <Link href="" className="font-bold text-gray-700 text-2xl">
-            OpenTable
-          </Link>
-          <div className="flex">
-            <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-              Sign In
-            </button>
-            <button className="border p-1 px-4 rounded text-black">
-              Sign Up
-            </button>
-          </div>
-        </nav>
-
-        <main>
-          {/* Header Component */}
-          <div className="h-64 bg-gradient-to-r from-[#0f1f47] to-[#5f6984] p-2">
-            <div className="text-center mt-10">
-              <h1 className="text-white text-5xl font-bold mb-2">
-                Find your table for any occasion
-              </h1>
-
-              {/* Search Bar Component */}
-              <div className="text-left text-lg py-3 m-auto flex justify-center">
-                <input
-                  className="rounded mr-3 p-2 w-[450px]"
-                  type="text"
-                  placeholder="State, City or Town"
-                />
-                <button className=" rounded bg-red-600 px-9 py-2 text-white">
-                  Let's Go
-                </button>
-              </div>
-            </div>
-          </div>
-
-          {/* Cards Component */}
-          <div className="py-3 px-36 mt-10 flex flex-wrap">
-            <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-              <Image src="" alt="" className="w-full h-36" />
-              <div className="p-1">
-                <h3 className="font-bold text-2xl mb-2 text-black">Milestones Grill</h3>
-              </div>
-            </div>
-          </div>
-        </main>
-      </main>
-    </main>
+<main>Home Page</main>
   );
 }
