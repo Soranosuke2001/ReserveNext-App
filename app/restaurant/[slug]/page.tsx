0079794ae758eab838631f7c5708701e0e89941f
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-07 13:52:23 -0700

    now fetches the restaurant clicked

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import Header from "./components/Header";
 import RestaurantNavBar from "./components/RestaurantNavBar";
 import Title from "./components/Title";
 import Ratings from "./components/Ratings";
@@ -7,8 +6,43 @@ import RestaurantImages from "./components/RestaurantImages";
 import RestaurantReviews from "./components/RestaurantReviews";
 import Reservation from "./components/Reservation";
 import Head from "./head";
+import { PrismaClient } from "@prisma/client";
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+
+  if (!restaurant) throw new Error();
+
+  return restaurant;
+};
+
+export default async function RestaurantDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <>
       <Head />
