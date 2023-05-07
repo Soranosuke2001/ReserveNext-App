2c55fc213136d7fb0f4a3a38883b08d6661bd20e
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-07 14:31:51 -0700

    error handling for no menu

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -7,17 +7,22 @@ interface MenuProps {
 }
 
 const Menu: FC<MenuProps> = ({ menu }) => {
+  const menuLength = menu.length;
   return (
     <main className="bg-white mt-5">
       <div>
         <div className="mt-4 pb-1 mb-1">
           <h1 className="font-bold text-4xl">Menu</h1>
         </div>
-        <div className="flex flex-wrap justify-between">
-          {menu.map((item) => (
-            <MenuCard item={item} />
-          ))}
-        </div>
+        {menuLength > 0 ? (
+          <div className="flex flex-wrap justify-between">
+            {menu.map((item) => (
+              <MenuCard item={item} />
+            ))}
+          </div>
+        ) : (
+          <h2>The Menu was not provided</h2>
+        )}
       </div>
     </main>
   );
