30f36db0bd29289c13002d612f85e7f99c48f06d
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-07 14:26:04 -0700

    dynamically renders the menu items

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -1,17 +1,22 @@
 import { FC } from "react";
 import MenuCard from "./MenuCard";
+import { Item } from "@prisma/client";
 
-interface MenuProps {}
+interface MenuProps {
+  menu: Item[];
+}
 
-const Menu: FC<MenuProps> = ({}) => {
+const Menu: FC<MenuProps> = ({ menu }) => {
   return (
     <main className="bg-white mt-5">
       <div>
         <div className="mt-4 pb-1 mb-1">
           <h1 className="font-bold text-4xl">Menu</h1>
         </div>
-        <div className="flex flex-wrap-justify-between">
-          <MenuCard />
+        <div className="flex flex-wrap justify-between">
+          {menu.map((item) => (
+            <MenuCard item={item} />
+          ))}
         </div>
       </div>
     </main>
