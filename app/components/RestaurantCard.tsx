e45064070fc9ddc9f56297999a097b2864c22295
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-07 13:29:41 -0700

    dynamically renders the list of restaurants

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -9,22 +9,30 @@ interface RestaurantCardProps {
 }
 
 const RestaurantCard: FC<RestaurantCardProps> = ({ restaurant }) => {
+  const { id, name, main_image, cuisine, location, price, slug } = restaurant;
   return (
-    <div className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer">
-      <Link href="/restaurant/milestones-grill">
-        <Image src="" alt="" className="w-full h-36" />
+    <div
+      key={id}
+      className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer"
+    >
+      <Link href={`/restaurant/${slug}`}>
+        <Image
+          src={main_image}
+          width={400}
+          height={400}
+          alt=""
+          className="w-full h-36"
+        />
         <div className="p-1">
-          <h3 className="font-bold text-2xl mb-2 text-black">
-            Milestones Grill
-          </h3>
+          <h3 className="font-bold text-2xl mb-2 text-black">{name}</h3>
           <div className="flex items-start">
             <div className="flex mb-2">*****</div>
             <p className="ml-2">112 Reviews</p>
           </div>
           <div className="flex text-reg font-light capitalize">
-            <p className="mr-3">Mexican</p>
+            <p className="mr-3">{cuisine.name}</p>
             <p className="mr-3">$$$$</p>
-            <p>Toronto</p>
+            <p>{location.name}</p>
           </div>
           <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
         </div>
