989707f189205bae54f3a81b4e07d3077a866c4d
Soranosuke2001 <sorazora1.4@gmail.com>
2023-05-09 15:08:36 -0700

    validated user inputs

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,58 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import validator from "validator";
+
+export default function POST(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "POST") {
+    const { firstName, lastName, email, phone, city, password } = req.body;
+
+    const errors: string[] = [];
+
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "First Name is too short or too long",
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "Last Name is too short or too long",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is Invalid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone Number is Invalid",
+      },
+      {
+        valid: validator.isLength(city, {
+          min: 1,
+        }),
+        errorMessage: "City is Invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not strong enough",
+      },
+    ];
+
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+
+    if (errors.length) {
+      return res.status(400).send({ errorMessage: errors[0] });
+    }
+
+    res.status(200).send({ message: "Complete" });
+  }
+}
