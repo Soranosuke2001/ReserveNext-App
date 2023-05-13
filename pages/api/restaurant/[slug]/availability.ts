2af8efa3749a4dd1b1a6223891bce877b8e3899c
sora <sorazora1.4@gmail.com>
2023-05-13 12:58:24 -0700

    returns the list of bookings from the search time array

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -1,5 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import { times } from "../../../../data";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 type QueryProps = {
   slug: string;
   day: string;
@@ -7,12 +12,40 @@ type QueryProps = {
   partySize: string;
 };
 
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
+export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   const { slug, day, time, partySize } = req.query as QueryProps;
 
   if (!day || !time || !partySize) {
     return res.status(400).send({ message: "Invalid Data Provided" });
   }
 
-  return res.status(200).send({ slug, day, time, partySize });
+  const searchTimes = times.find((t) => {
+    return t.time === time;
+  })?.searchTimes;
+
+  console.log();
+
+  if (!searchTimes) {
+    return res.status(400).send({
+      message: "Invalid Data Provided searchTimes",
+    });
+  }
+
+  const bookings = await prisma.booking.findMany({
+    where: {
+      booking_time: {
+        gte: new Date(`${day}T${searchTimes[0]}`),
+        lte: new Date(`${day}T${searchTimes[searchTimes.length - 1]}`),
+      },
+    },
+    select: {
+      number_of_people: true,
+      booking_time: true,
+      tables: true,
+    },
+  });
+
+  return res.status(200).send({ searchTimes, bookings });
 }
+
+// http://localhost:3000/api/restaurant/vivaan-fine-indian-cuisine-ottawa/availability?day=2023-02-03&time=14:00:00.000Z&partySize=4
